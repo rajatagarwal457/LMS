fbce9176d6cfd6c9c1c25bb48559452019af1c6f
anjanvikas <anjanvikas2001@gmail.com>
2023-09-19 00:05:39 +0530

    [Added] Admin Loan Card Management

---
+++ Repository/AdminLoanCardManagementProvider.cs
@@ -0,0 +1,43 @@
+﻿using LMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Data
+{
+    public class AdminLoanCardManagementProvider : IAdminLoanCardManagementProvider
+    {
+        private readonly LmsContext _context;
+
+        public AdminLoanCardManagementProvider(LmsContext context)
+        {
+            _context = context;
+        }
+        public async Task<LoanCardMaster> GetLoanCardByIdAsync(string loanId)
+        {
+            return await _context.LoanCardMasters.FindAsync(loanId);
+        }
+        public async Task AddLoanCardAsync(LoanCardMaster loanCard)
+        {
+            _context.LoanCardMasters.Add(loanCard);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<LoanCardMaster>> GetAllLoanCardsAsync()
+        {
+            return await _context.LoanCardMasters.ToListAsync();
+        }
+        public async Task UpdateLoanCardAsync(LoanCardMaster loanCard)
+        {
+            _context.Entry(loanCard).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteLoanCardAsync(string loanId)
+        {
+            var _loancard = await _context.LoanCardMasters.FindAsync(loanId);
+            if (_loancard != null) 
+            {
+                _context.LoanCardMasters.Remove(_loancard);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Repository/IAdminLoanCardManagementProvider.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Models;
+
+namespace LMS.Data
+{
+    public interface IAdminLoanCardManagementProvider
+    {
+        Task<LoanCardMaster> GetLoanCardByIdAsync(string loanId);
+
+        Task AddLoanCardAsync(LoanCardMaster loanCard);
+        Task<List<LoanCardMaster>> GetAllLoanCardsAsync();
+        Task UpdateLoanCardAsync(LoanCardMaster loanCard);
+        Task DeleteLoanCardAsync(string loanId);
+    }
+}
